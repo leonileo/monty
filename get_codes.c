6cd4d3fd9b0c283647b6ea2b563720a094459d93
kaleb <kalebwendwessen@gmail.com>
2024-03-31 13:47:04 -0700

    monty

---
+++ get_codes.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * get_opcodes - selects the correct opcode to perform
+ *
+ * @opc: opcode passed
+ *
+ * Return: pointer to the function that executes the opcode
+ */
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t instruct[] = {
+		{"push", _push},
+		{"pall", _pall},
+
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; instruct[i].opcode; i++)
+	{
+		if (_strcmp(instruct[i].opcode, opc) == 0)
+			break;
+	}
+
+	return (instruct[i].f);
+}
