ed8ecbcde147206dd7b307b254e136b957a31644
kaleb <kalebwendwessen@gmail.com>
2024-03-31 13:34:15 -0700

    monty

--- monty.h
+++ monty.h
@@ -54,8 +54,8 @@ typedef struct instruction_s
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
+void f_push(stack_t **head, unsigned int cline)
+void f_pall(stack_t **head, unsigned int cline);
 void f_pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
